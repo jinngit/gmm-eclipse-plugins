4eaf11aff66686df92dd4a4de7848a4d7007c20d
Vincent Aranega <vincent.aranega@axellience.com>
2014-07-21 15:00:29 +0200

    Add abstract handler and some util methods

--- org.genmymodel.engine.connector/src/org/genmymodel/engine/connector/handlers/GMMCompileHandler.java
+++ org.genmymodel.engine.connector/src/org/genmymodel/engine/connector/handlers/GMMCompileHandler.java
@@ -1,18 +1,20 @@
 package org.genmymodel.engine.connector.handlers;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
- * Our sample handler extends AbstractHandler, an IHandler base class.
- * @see org.eclipse.core.commands.IHandler
- * @see org.eclipse.core.commands.AbstractHandler
+ * This class provides handler calling GenMyModel API. The GenMyModel service
+ * called allows one to compile its project.
+ * 
+ * @author Vincent Aranega
+ *
  */
-public class GMMCompileHandler extends AbstractHandler {
+public class GMMCompileHandler extends GMMAbstractHandler {
+
 	/**
 	 * The constructor.
 	 */
@@ -24,11 +26,15 @@ public class GMMCompileHandler extends AbstractHandler {
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"GenMyModel Engine Connector",
-				"Compilation");
+		super.execute(event);
+		IWorkbenchWindow window = HandlerUtil
+				.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(window.getShell(),
+				"GenMyModel Engine Connector", "Compilation");
+
+		System.out.println("PROJECT " + extractProject());
+
 		return null;
 	}
+
 }
--- org.genmymodel.engine.connector/src/org/genmymodel/engine/connector/handlers/GMMCompileLaunchHandler.java
+++ org.genmymodel.engine.connector/src/org/genmymodel/engine/connector/handlers/GMMCompileLaunchHandler.java
@@ -1,18 +1,17 @@
 package org.genmymodel.engine.connector.handlers;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
-public class GMMCompileLaunchHandler extends AbstractHandler {
+public class GMMCompileLaunchHandler extends GMMAbstractHandler {
 	/**
 	 * The constructor.
 	 */
