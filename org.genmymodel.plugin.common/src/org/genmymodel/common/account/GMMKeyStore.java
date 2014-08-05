1e0612deb53e891ccd62a01781179edcc50396fb
ali gourch <ali.gourch@axellience.com>
2014-08-05 16:51:44 +0200

    First stable version of GenMyModel explorer

---
+++ org.genmymodel.plugin.common/src/org/genmymodel/common/account/GMMKeyStore.java
@@ -0,0 +1,63 @@
+package org.genmymodel.common.account;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  This class manage the keyStore.
+ * @author Ali Gourch
+ */
+public class GMMKeyStore {
+
+	public static Map<String, GMMCredential> CREDENTIALS;
+	public static GMMCredential CREDENTIAL;
+	public static GMMKeyStore KEYSTORE = new GMMKeyStore();
+	
+	/**
+	 * Prevents external instance creation.
+	 */
+	private GMMKeyStore() {
+		initialize();
+	}
+	
+	synchronized protected void initialize() {
+		GMMKeyStore.CREDENTIALS = new HashMap<String, GMMCredential>();
+	}
+	
+	/**
+	 * Gets the GMMKeyStore instance.
+	 * @return The GMMKeyStore instance.
+	 */
+	public static GMMKeyStore getInstance() {
+		return KEYSTORE;
+	}
+	
+	/**
+	 * Adds the GMMKeyStore credential.
+	 */
+	synchronized public void addCredential(String username, GMMCredential credential) {
+		GMMKeyStore.CREDENTIALS.put(username, credential);
+	}
+	
+	/**
+	 * Removes the GMMKeyStore credential.
+	 */
+	synchronized public void removeCredential(String username) {
+		GMMKeyStore.CREDENTIALS.remove(username);
+	}
+	
+	/**
+	 * gets the GMMKeyStore credential.
+	 * @return The current credential.
+	 */
+	synchronized public GMMCredential getCurrentCredential() {
+		return GMMKeyStore.CREDENTIAL;
+	}
+	
+	/**
+	 * Loads the GMMKeyStore credential.
+	 */
+	synchronized public void loadCredential(String username) {
+		GMMKeyStore.CREDENTIAL = GMMKeyStore.CREDENTIALS.get(username);	
+	}
+}
