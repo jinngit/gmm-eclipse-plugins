b27e4865b8a6195890b3c7e0afbb524ab81be1d4
ali gourch <ali.gourch@axellience.com>
2014-08-14 15:40:34 +0200

    Add ProjectPostBinding.java

---
+++ org.genmymodel.plugin.common/src/org/genmymodel/common/api/ProjectPostBinding.java
@@ -0,0 +1,25 @@
+package org.genmymodel.common.api;
+
+/**
+ * @author Ali Gourch
+ *
+ */
+public class ProjectPostBinding extends ProjectBinding
+{
+	private byte[] base64data;
+
+	/**
+	 * @return the data
+	 */
+	public byte[] getData()
+	{
+		return base64data;
+	}
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(byte[] base64data)
+	{
+		this.base64data = base64data;
+	}
+}
